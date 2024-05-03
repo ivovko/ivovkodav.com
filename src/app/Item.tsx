fbc3319c81b97bf80cc32b2bfb15f84a83127f5b
Ihor Vovkodav <igorvovkodav159@gmail.com>
2024-05-03 18:23:34 +0200

    Item added

---
+++ src/app/Item.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface ItemProps {
+  content: string;
+}
+
+export const Item: React.FC<ItemProps> = ({ content }) => {
+  return (
+    <svg id="svgelem" width="310" height="60">
+      <defs>
+        <linearGradient id="item-grad">
+          <stop offset="0%" stop-color="#FF00EE" />
+          <stop offset="15%" stop-color="#BB00AF" />
+          <stop offset="40%" stop-color="#6F0067" />
+          <stop offset="60%" stop-color="#6F0067" />
+          <stop offset="85%" stop-color="#BB00AF" />
+          <stop offset="100%" stop-color="#FF00EE" />
+        </linearGradient>
+      </defs>
+      <polygon points="0,50 0,20 20,0 300,0 300,30 280,50" className="item" />
+      <text
+        x="50%"
+        y="50%"
+        dominant-baseline="middle"
+        text-anchor="middle"
+        fontSize={40}
+        className="itemText"
+      >
+        {content}
+      </text>
+    </svg>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Item } from "./Item";
 
 export default function MainPage() {
   return (
     <main>
       <h5 className="main_name">Ihor Vovkodav</h5>
+      <Item content={new Date().toLocaleDateString()} />
     </main>
   );
 }
