d9e207203de03f53ffb821a72e99658c8f1d01da
Ihor Vovkodav <igorvovkodav159@gmail.com>
2024-06-19 21:27:35 +0200

    db connection fix 7

--- src/db/db-api.ts
+++ src/db/db-api.ts
@@ -10,9 +10,9 @@ export async function getDBClient() {
   }
 
   db = createPool({
-    host: "localhost",
+    host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
-    port: 3306,
+    port: +process.env.MYSQL_PORT!,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
   });
